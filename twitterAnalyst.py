92aedd0d05c6c5b7088b1e49fa38862f5a558cd7
EZPK <thom.bruyer@gmail.com>
2017-11-19 15:48:21 +0100

    Update twitterAnalyst.py

--- twitterAnalyst.py
+++ twitterAnalyst.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = 'i9AA2Bel71Ls2gAwgRnvVdVyg'
-consumer_secret = 'mVSIAJ6sye0EYUp5itwCQEvP0yH75v0DXN5oM6f3H50KvmvDKQ'
+consumer_key = 'secret'
+consumer_secret = 'encore plus secret'
 
-acces_token = '1251785250-bM0wi2GnCOIcqHHbKz0XCwiTZNO3vtRMpSq34rh'
-acces_token_secret = 'VK89GhtMpNCm5b6NY1fgVmT7en7kd2qFS47vMtq2ZIpfp'
+acces_token = 'terriblement secret'
+acces_token_secret = 'OMG classified files for sure'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(acces_token, acces_token_secret)
